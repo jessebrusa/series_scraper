77f812a72fed89b117e419a7a59e1dcc1321d1ab
jessebrusa <jessebrusadev@gmail.com>
2025-02-20 21:23:58 -0500

    directories can be created with file manager

--- src/file_manager.py
+++ src/file_manager.py
@@ -4,13 +4,27 @@ class FileManager:
     def __init__(self):
         self.mount_point = "/Volumes/Plex Movies and Shows/Anime"
 
+    def create_series_directory(self, series_title):
+        self.series_path = os.path.join(self.mount_point, series_title)
+        os.makedirs(self.series_path, exist_ok=True)
+
+    def create_seasons_directory(self, num_seasons):
+        season_int = int(num_seasons)
+        for season in range(1, season_int + 1):
+            season_path = os.path.join(self.mount_point, f"{self.series_path}/Season {season:02}")
+            os.makedirs(season_path, exist_ok=True)
+
     def write_file(self, file_path, data):
         full_path = os.path.join(self.mount_point, file_path)
         with open(full_path, 'wb') as file:
             file.write(data)
 
-
+# Example usage
 if __name__ == "__main__":
     file_manager = FileManager()
-    file_manager.write_file("example.txt", b"Hello, NAS!")
 
+    # Create series directory
+    file_manager.create_series_directory("Example Series")
+
+    # Create seasons directory
+    file_manager.create_seasons_directory("3")
